dcc4edddf2f3a80d265762dc94c9b0826397396c
Tyler Holmes <HolmesSoftwareDev@gmail.com>
2024-06-29 17:10:53 -0600

    Found issue when rematching player.
    Added js code when rematching someone, im not going to use it tho. I dont like the idea of having it run onClick. CSS only solution wasnt working. Ill look into it more next time it comes up i guess

--- content.js
+++ content.js
@@ -1,2 +1,18 @@
 // content.js
-console.log("Hide Elo extension loaded");
+console.log("Hide Elo extension loaded ♟️");
+
+// // Chat room code for when you play a rematch
+// const chatRoomComponent = document.querySelector(".chat-room-component");
+
+// document.addEventListener("click", function (event) {
+// 	const spans = chatRoomComponent.querySelectorAll("span");
+
+// 	if (!chatRoomComponent) return;
+// 	if (!spans) return;
+
+// 	spans.forEach((span) => {
+// 		if (span.textContent.includes("rating")) {
+// 			span.remove();
+// 		}
+// 	});
+// });
